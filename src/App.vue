f7b08a884c8f3a00e69fd3f29f741979e0aafc54
service-crimea <develop.service-crimea@mail.ru>
2022-09-04 09:57:42 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <search-component/>
+  </div>
+</template>
+
+<script>
+import SearchComponent from "@/components/SearchComponent";
+export default {
+  components: {
+    SearchComponent
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>{{ textSearch }} (length: {{ textSearch.length }}) </h3>
+    <input type="text" size="50" v-model="textSearch">
+    <button type="button" @click="getSearch">грузи</button>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'search_component',
+  data() {
+    return {
+      textSearch: ''
+    }
+  },
+  watch: {
+    textSearch: function() {
+      if ( this.textSearch.length > 3 ) {
+        this.textSearch = 'chenge text on the field'
+      }
+    }
+  },
+  methods: {
+    getSearch() {
+      console.log("")
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
